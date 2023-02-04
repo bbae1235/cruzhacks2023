448aa44d81dfe08f9c8bfacd05988656bacb7bda
dbeltra4 <124412754+dbeltra4@users.noreply.github.com>
2023-02-04 20:39:04 +0000

    image added

--- ucscgo/App.js
+++ ucscgo/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native'; // Added platform
+
+const ucscImage = require('./assets/UCSCGo.png');
+///workspaces/cruzhacks2023/ucscgo/assets/UCSCGo.png
+//<Image source={require('./img/workspaces/cruzhacks2023/ucscgo/assets/ucscgobackground2.0.png')} />
 
 export default function App() {
+  //console.log("App started"); //This is like a print statement to your terminal
+  ///Pathway: /workspaces/cruzhacks2023/ucscgo/assets/ucscgobackground2.0.png
   return (
     <View style={styles.container}>
-      <Text style={styles.setFontSizeThree}>Welcome to UCSC Go!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={ucscImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +25,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  setFontSizeThree: {
-    fontSize: 25 // Define font size here in Pixels
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  //setFontSizeThree: {
+    //fontSize: 25 // Define font size here in Pixels
+  //},
 });
