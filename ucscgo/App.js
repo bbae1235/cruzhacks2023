5ffbde68d845bd1a4c9cb0f1058c4eed99bccba0
dbeltra4 <124412754+dbeltra4@users.noreply.github.com>
2023-02-05 01:25:26 +0000

    navigation implemented

--- ucscgo/App.js
+++ ucscgo/App.js
@@ -1,33 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet, Text, View, Image } from 'react-native'; // Added platform
 
+//This is from react navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
 //These are the components
 import ImageViewer from './components/ImageViewer'; //homescreen image
-//import Button from './components/Button';
+import Button from './components/Button';
 
-//const ucscImage = require('./assets/UCSCGo.png');
-const placeholderImage = require('./assets/UCSCGo.png');
-
-
-{/* This is the beginning of the App function */}
-export default function App() {
-{/* console.log("App started"); //This is like a print statement to your terminal */}
-{/* Pathway: /workspaces/cruzhacks2023/ucscgo/assets/ucscgobackground2.0.png */}
+//This is the HomeScreen
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={placeholderImage} /> 
-      </View> 
-      {/*<View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} /> 
-      </View>*/}
+      </View>
+      {/*app name*/}
+    <View style={styles.title}>
+      <Text style={[styles.setFontSizeFour, styles.setColorYellow]}>UCSC Go!</Text>
+      <StatusBar style="auto" />
+    </View>
+      {/*Buttons*/}
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="UCSC Landmarks" />
+      <Button theme="primary" label="UCSC Study Rooms" />
+      </View>
+  
       <StatusBar style = "auto" />
     </View>
   );
 }
 
+//This is the Santa Cruz Landmarks
+function SCLandMark() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+//const ucscImage = require('./assets/UCSCGo.png');
+const placeholderImage = require('./assets/UCSCGo.png');
+
+//The Native Stack Navigator variable
+const Stack = createNativeStackNavigator();
+
+
+{/* This is the beginning of the App function */}
+export default function App() {
+{/* console.log("App started"); //This is like a print statement to your terminal */}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="UCSC Landmarks" component={SCLandMark} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
+  //container for whole screen
   container: {
     flex: 1,
     flexShrink: 0,
@@ -40,5 +78,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // Top-Bottom
     justifyContent: 'flex-start',
+  },
+  //container for buttons
+  footerContainer: {
+    // Button POS
+    flex: 2, 
+    flexShrink: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  setFontSizeFour: {
+    fontSize: 30 // Define font size here in Pixels
+  },
+  setColorYellow :{
+    color: '#FFFF00'
   },
 })
